6964ccba54b807fddff4ddc681aee85584259f76
Denis Woldemariam <damewold@gmail.com>
2020-09-16 18:09:40 -0500

    created a new footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -62,6 +62,7 @@ class App extends Component {
        
      render(){
     return(
+      <>
       <div className="App">
          <header className="App-header">
                 <h1 className="App-title">MY STICKY NOTES</h1>
@@ -93,6 +94,10 @@ class App extends Component {
            </div>
            </div>
             </div>
+             <footer>
+          
+             </footer>
+             </>
           
                 
     )
