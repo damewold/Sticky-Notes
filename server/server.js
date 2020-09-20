cf1530dfd1dc2eb8b45346fc5212b90bb99735f0
Denis Woldemariam <damewold@gmail.com>
2020-09-19 20:48:03 -0500

    make changes to the server to fix deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
@@ -7,17 +6,6 @@ const PORT = process.env.PORT || 5000;
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
-
-
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('client/build'));
-// }
-
-// app.get('*', (request, response) => {
-// 	response.sendFile(path.join(__dirname+'client/build/index.html'));
-// });
 
 /**------Express routes------------ */
 const router = require('./routes/taskRouter');
